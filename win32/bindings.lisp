4797dc18a9c301c3aa73b7583b4ed5c70b1be2e0
Shinmera <shinmera@tymoon.eu>
2024-03-12 19:59:16 +0100

    bla

--- win32/bindings.lisp
+++ win32/bindings.lisp
@@ -6,59 +6,158 @@
 (cffi:define-foreign-library shcore
   (T (:defaults "shcore")))
 
-(cffi:defcfun ("AdjustWindowRect") com:hresult
-  )
-(cffi:defcfun ("BitBlt") com:hresult
-  )
-(cffi:defcfun ("ChangeDisplaySettings") com:hresult
-  )
-(cffi:defcfun ("CreateWindowEx") com:hresult
-  )
-(cffi:defcfun ("DefWindowProc") com:hresult
-  )
-(cffi:defcfun ("DestroyWindow") com:hresult
-  )
-(cffi:defcfun ("DispatchMessage") com:hresult
-  )
-(cffi:defcfun ("EnableNonClientDpiScaling") com:hresult
-  )
-(cffi:defcfun ("EnumDisplaySettings") com:hresult
-  )
-(cffi:defcfun ("GetDC") com:hresult
-  )
+(cffi:defcenum monitor-dpi-type
+  (:effective-dpi 0)
+  (:angular-dpi 1)
+  (:raw-dpi 2))
+
+(cffi:defcenum dpi-awareness
+  (:unaware 0)
+  (:system-dpi-aware 1)
+  (:per-monitor-dpi-aware 2))
+
+(cffi:defcfun ("AdjustWindowRect") :boolean
+  (rect :pointer)
+  (style :int32)
+  (menu :boolean))
+
+(cffi:defcfun ("BitBlt") :bool
+  (hdc :pointer)
+  (x :int)
+  (y :int)
+  (cx :int)
+  (cy :int)
+  (hdc-src :pointer)
+  (x1 :int)
+  (y1 :int)
+  (rop :int32))
+
+(cffi:defcfun ("ChangeDisplaySettingsW") :long
+  (dev-mode :pointer)
+  (flags :int32))
+
+(cffi:defcfun ("CreateWindowExW") :pointer
+  (ex-style :int32)
+  (class-name :pointer)
+  (window-name :pointer)
+  (style :int32)
+  (x :int)
+  (y :int)
+  (w :int)
+  (h :int)
+  (parent :pointer)
+  (menu :pointer)
+  (instance :pointer)
+  (param :pointer))
+
+(cffi:defcfun ("DefWindowProcW") :size
+  (window :pointer)
+  (message :uint)
+  (wparameter :size)
+  (lparameter :size))
+
+(cffi:defcfun ("DestroyWindow") :boolean
+  (window :pointer))
+
+(cffi:defcfun ("DispatchMessageW") :size
+  (message :pointer))
+
+(cffi:defcfun ("EnableNonClientDpiScaling") :boolean
+  (window :pointer))
+
+(cffi:defcfun ("EnumDisplaySettingsW") :boolean
+  (device-name :pointer)
+  (mode-num :int32)
+  (dev-mode :pointer))
+
+(cffi:defcfun ("GetDC") :pointer
+  (window :pointer))
+
 (cffi:defcfun ("GetDpiForMonitor") com:hresult
-  )
-(cffi:defcfun ("GetDpiForWindow") com:hresult
-  )
-(cffi:defcfun ("GetMessageTime") com:hresult
-  )
-(cffi:defcfun ("GetSystemMetrics") com:hresult
-  )
-(cffi:defcfun ("GetWindowLongPtr") com:hresult
-  )
-(cffi:defcfun ("InvalidateRect") com:hresult
-  )
-(cffi:defcfun ("PeekMessageW") com:hresult
-  )
-(cffi:defcfun ("RegisterClass") com:hresult
-  )
-(cffi:defcfun ("ReleaseDC") com:hresult
-  )
-(cffi:defcfun ("SendMessage") com:hresult
-  )
-(cffi:defcfun ("SetProcessDPIAware") com:hresult
-  )
-(cffi:defcfun ("SetProcessDpiAwarenessContext") com:hresult
-  )
-(cffi:defcfun ("SetWindowLongPtr") com:hresult
-  )
-(cffi:defcfun ("SetWindowPos") com:hresult
-  )
-(cffi:defcfun ("ShowWindow") com:hresult
-  )
-(cffi:defcfun ("StretchDIBits") com:hresult
-  )
-(cffi:defcfun ("TranslateMessage") com:hresult
-  )
-(cffi:defcfun ("ValidateRect") com:hresult
-  )
+  (monitor :pointer)
+  (dpi-type monitor-dpi-type)
+  (x :pointer)
+  (y :pointer))
+
+(cffi:defcfun ("GetDpiForWindow") :uint
+  (pointer :window))
+
+(cffi:defcfun ("GetMessageTime") :long)
+
+(cffi:defcfun ("GetSystemMetrics") :int
+  (index :int))
+
+(cffi:defcfun ("GetWindowLongPtrW") :ssize
+  (window :pointer)
+  (index :int))
+
+(cffi:defcfun ("InvalidateRect") :boolean
+  (window :pointer)
+  (rect :pointer)
+  (erase :boolean))
+
+(cffi:defcfun ("PeekMessageW") :boolean
+  (message :pointer)
+  (window :pointer)
+  (message-filter-min :uint)
+  (message-filter-max :uint)
+  (remove-message :uint))
+
+(cffi:defcfun ("RegisterClassW") :int16
+  (class :pointer))
+
+(cffi:defcfun ("ReleaseDC") :int
+  (window :pointer)
+  (dc :pointer))
+
+(cffi:defcfun ("SendMessageW") :ssize
+  (window :pointer)
+  (message :uint)
+  (wparameter :size)
+  (lparameter :size))
+
+(cffi:defcfun ("SetProcessDPIAware") :boolean)
+
+(cffi:defcfun ("SetProcessDpiAwarenessContext") :boolean
+  (context :pointer))
+
+(cffi:defcfun ("SetProcessDpiAwareness") com:hresult
+  (awareness dpi-awareness))
+
+(cffi:defcfun ("SetWindowLongPtrW") :ssize
+  (window :pointer)
+  (index :int)
+  (new-long :ssize))
+
+(cffi:defcfun ("SetWindowPos") :boolean
+  (window :pointer)
+  (insert-after :pointer)
+  (x :int)
+  (y :int)
+  (cx :int)
+  (cy :int)
+  (flags :uint))
+
+(cffi:defcfun ("ShowWindow") :boolean
+  (window :pointer)
+  (cmd-show :int))
+
+(cffi:defcfun ("StretchDIBits") :int
+  (window :pointer)
+  (xdest :int)
+  (ydest :int)
+  (wdest :int)
+  (hdest :int)
+  (xsrc :int)
+  (ysrc :int)
+  (wsrc :int)
+  (hsrc :int)
+  (rop :int32))
+
+(cffi:defcfun ("TranslateMessage") :boolean
+  (message :pointer))
+
+(cffi:defcfun ("ValidateRect") :boolean
+  (window :pointer)
+  (rect :pointer))
+

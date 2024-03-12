b9500eb209581ce64dc75d2473ee3bea7faf5098
Shinmera <shinmera@tymoon.eu>
2024-03-12 22:52:28 +0100

    aaaa

--- win32/wrapper.lisp
+++ win32/wrapper.lisp
@@ -13,6 +13,9 @@
 (define-condition win32-error (fb:framebuffer-error com:win32-error)
   ())
 
+(defun win32-error (&key function-name message)
+  (com:win32-error T :function-name function-name :message message :type 'win32-error))
+
 (defmethod fb-int:init-backend ((backend (eql :win32)))
   (unless (cffi:foreign-library-loaded-p 'win32:user32)
     (cffi:load-foreign-library 'win32:user32)
@@ -26,11 +29,33 @@
 (defmethod fb-int:shutdown-backend ((backend (eql :win32)))
   (com:shutdown))
 
-(defmethod fb-int:open-backend ((backend (eql :win32)) &key))
+(defmethod fb-int:open-backend ((backend (eql :win32)) &key (title (fb-int:default-title)) location size (visible-p T))
+  (let* ((screen-w (win32:get-system-metrics :cx-screen))
+         (screen-h (win32:get-system-metrics :cy-screen))
+         (w (or (car size) screen-w))
+         (h (or (cdr size) screen-h))
+         (x (or (car location) (round (- screen-w w) 2)))
+         (y (or (cdr location) (round (- screen-h h) 2)))
+         (style (list :maximizebox :sizebox))
+         (class (cffi:foreign-alloc '(:struct win32:window-class))))
+    (setf (win32:window-class-style class) '(:owndc :vredraw :hredraw))
+    (setf (win32:window-class-proc class) (cffi:callback handle-event))
+    (setf (win32:window-class-cursor class) (win32:load-cursor 0 :arrow))
+    (setf (win32:window-class-class-name class) "framebuffer")
+    (win32:register-class class)
+    (let ((ptr (win32:create-window 0 "framebuffer" title style x y w h 0 0 0 0)))
+      (when (cffi:null-pointer-p ptr)
+        (win32-error :function-name 'win32:create-window))
+      (make-instance 'window :ptr ptr 
+                             :dc (win32:get-dc ptr)
+                             :size (cons w h)
+                             :location (cons x y)
+                             :visible-p visible-p))))
 
 (defclass window (fb:window)
   ((ptr :initarg :ptr :accessor ptr)
    (dc :initarg :dc :accessor dc)
+   (class :initarg :class :accessor class)
    (buffer :reader fb:buffer :accessor buffer)
    (close-requested-p :initform NIL :initarg :close-requested-p :accessor fb:close-requested-p :accessor close-requested-p)
    (size :initform (cons 1 1) :initarg :size :reader fb:size :accessor size)
@@ -40,6 +65,13 @@
    (maximized-p :initform NIL :initarg :maximized-p :reader fb:maximized-p :accessor maximized-p)
    (iconified-p :initform NIL :initarg :iconified-p :reader fb:iconified-p :accessor iconified-p)
    (content-scale :initform (cons 1 1) :initarg :content-scale :reader fb:content-scale :accessor content-scale)))
+
+(defmethod initialize-instance :after ((window window) &key)
+  (let ((ptr (ptr window)))
+    (setf (fb-int:ptr-window ptr) window)
+    (win32:set-window ptr :userdata ptr)
+    (when visible-p
+      (win32:show-window ptr :normal))))
 
 (defmethod fb:valid-p ((window window))
   (not (null (ptr window))))

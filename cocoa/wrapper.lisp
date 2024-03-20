86d44380bb731ec8a2718d635143e650b05c592e
Shinmera <shinmera@tymoon.eu>
2024-03-20 11:38:51 +0100

    Idk

--- cocoa/wrapper.lisp
+++ cocoa/wrapper.lisp
@@ -26,6 +26,70 @@
                            :size (cons w h)
                            :location (cons x y))))
 
+(defmacro define-objc-class (name super &body methods)
+  `(objc::define-objc-class ,name ,super
+     ,@(loop for (name rettype args . body) in methods
+             collect `(,name ,rettype ,args
+                             (let ((window (fb-int:ptr-window objc::self)))
+                               (declare (ignorable window))
+                               ,@body)))))
+
+(trivial-indent:define-indentation define-objc-class
+    (4 4 &rest (&whole 2 6 6 &body)))
+
+(define-objc-class "FBWindow" "NSWindow"
+  (window-should-close/ :bool ((sender :id))
+    (fb:window-closed window)
+    NIL)
+  (window-did-resize/ :void ((notification :pointer)))
+  (window-did-move/ :void ((notification :pointer)))
+  (window-did-miniaturize/ :void ((notification :pointer)))
+  (window-did-deminiaturize/ :void ((notification :pointer)))
+  (window-did-become-key/ :void ((notification :pointer)))
+  (window-did-resign-key/ :void ((notification :pointer)))
+  (can-become-key-window :bool () T)
+  (can-become-main-window :bool () T))
+
+(define-objc-class "FBContentView" "NSView"
+  (can-become-key-view :bool () T)
+  (accepts-first-responder :bool () T)
+  (wants-update-layer :bool () T)
+  (update-layer :void ()
+    (fb:window-refreshed window))
+  (cursor-update/ :void ((event :pointer)))
+  (accepts-first-mouse/ :bool ((event :pointer)) T)
+  (mouse-down/ :void ((event :pointer)))
+  (mouse-dragged/ :void ((event :pointer)))
+  (mouse-up/ :void ((event :pointer)))
+  (mouse-moved/ :void ((event :pointer)))
+  (right-mouse-down/ :void ((event :pointer)))
+  (right-mouse-dragged/ :void ((event :pointer)))
+  (right-mouse-up/ :void ((event :pointer)))
+  (other-mouse-down/ :void ((event :pointer)))
+  (other-mouse-dragged/ :void ((event :pointer)))
+  (other-mouse-up/ :void ((event :pointer)))
+  (mouse-exited/ :void ((event :pointer)))
+  (mouse-entered/ :void ((event :pointer)))
+  (scroll-wheel/ :void ((event :pointer)))
+  (view-did-change-backing-properties :void ())
+  (draw-rect/ :void ((rect :id)))
+  (update-tracking-areas :void ())
+  (key-down/ :void ((event :pointer)))
+  (flags-changed/ :void ((event :pointer)))
+  (key-up/ :void ((event :pointer)))
+  (dragging-entered/ :id ((sender :id)))
+  (perform-drag-operation/ :bool ((sender :id)))
+  (has-marked-text :bool ())
+  (marked-range :id ())
+  (selected-range :id ())
+  (set-marked-text/selected-range/replacement-range/ :void ((string :id) (selected-range :id) (replacement-range :id)))
+  (unmark-text :void ())
+  (valid-attributes-for-marked-text :pointer ())
+  (attributed-substring-for-proposed-range/actual-range/ :pointer ((range :id) (actual-range :pointer)))
+  (character-index-for-point :uint ((point :id)) 0)
+  (first-rect-for-character-range/actual-range/ :pointer ((range :id) (actual-range :pointer)))
+  (insert-text/replacement-range/ :void ((string :id) (replacement-range :id))))
+
 (defclass window (fb:window)
   ((ptr :initarg :ptr :initform NIL :accessor ptr)
    (view :initarg :view :initform NIL :accessor view)
@@ -134,14 +198,6 @@
   (cocoa:set-level (ptr window) (if value :floating-window-level :normal-window-level))
   (setf (fb-int:floating-p window) value))
 
-(defmethod (setf fb:fullscreen-p) ((value null) (window window))
-  ;; TODO: implement fullscreen-p
-  )
-
-(defmethod (setf fb:fullscreen-p) ((value fb:video-mode) (window window))
-  ;; TODO: implement fullscreen-p
-  )
-
 (defmethod fb:clipboard ((window window))
   (objc:with-objects ((pasteboard (cocoa:nspasteboard-general-pasteboard)))
     (dolist (type (cocoa:types pasteboard))
@@ -178,8 +234,14 @@
   )
 
 (defmethod fb:process-events ((window window) &key timeout)
-  ;; TODO: implement process-events
-  )
+  (let ((s (etypecase timeout
+             ((eql T) 1.0d0)
+             (real (float timeout 0d0))
+             (null 0.0d0))))
+    (loop while (and (ptr window) (not (fb:close-requested-p window)))
+          do (cocoa:process-events :timeout s)
+             (unless (eql T timeout)
+               (return)))))
 
 (defmethod fb:request-attention ((window window))
   (cocoa:nsapp-request-user-attention :informational-request))
@@ -193,8 +255,10 @@
   )
 
 (defmethod fb:display ((window window))
-  ;; TODO: implement display
-  )
+  (typecase (fb:fullscreen-p window)
+    (null (call-next-method))
+    (display (fb:fullscreen-p window))
+    (video-mode (fb:display (fb:fullscreen-p window)))))
 
 (defmethod fb-int:list-displays-backend ((backend (eql :cocoa)))
   ;; TODO: implement list-displays-backend
@@ -211,4 +275,12 @@
 
 (defmethod fb:video-mode ((display display))
   ;; TODO: implement video-mode
+  )
+
+(defmethod (setf fb:fullscreen-p) ((value null) (window window))
+  ;; TODO: implement fullscreen-p
+  )
+
+(defmethod (setf fb:fullscreen-p) ((value fb:video-mode) (window window))
+  ;; TODO: implement fullscreen-p
   )

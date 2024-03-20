b11b76a723231f47dce5c536d6f4c92e83696ce3
Shinmera <shinmera@tymoon.eu>
2024-03-20 13:58:50 +0100

    argblargl

--- cocoa/bindings.lisp
+++ cocoa/bindings.lisp
@@ -1,1 +1,15 @@
 (in-package #:org.shirakumo.framebuffers.cocoa.cffi)
+
+(cffi:defcstruct (point :conc-name point-)
+  (x objc:cgfloat)
+  (y objc:cgfloat))
+
+(cffi:defcstruct (size :conc-name size-)
+  (w objc:cgfloat)
+  (h objc:cgfloat))
+
+(cffi:defcstruct (rect :conc-name rect-)
+  (x objc:cgfloat)
+  (y objc:cgfloat)
+  (w objc:cgfloat)
+  (h objc:cgfloat))
--- cocoa/package.lisp
+++ cocoa/package.lisp
@@ -1,5 +1,9 @@
 (defpackage #:org.shirakumo.framebuffers.cocoa.cffi
   (:use #:cl)
+  (:local-nicknames
+   (#:fb #:org.shirakumo.framebuffers)
+   (#:fb-int #:org.shirakumo.framebuffers.int)
+   (#:objc #:org.shirakumo.cocoas))
   (:export))
 
 (defpackage #:org.shirakumo.framebuffers.cocoa

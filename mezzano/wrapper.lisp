1dd5ba1c207ffc54f6c4b92eca7b7f492c9e9c51
Shinmera <shinmera@tymoon.eu>
2024-03-21 20:36:11 +0100

    Start on the mezzano backend.

--- mezzano/wrapper.lisp
+++ mezzano/wrapper.lisp
@@ -14,16 +14,33 @@
   ;; TODO: implement shutdown-backend
   )
 
-(defmethod fb-int:open-backend ((backend (eql :mezzano)) &key)
-  ;; TODO: implement list-displays-backend
-  )
+(defmethod fb-int:open-backend ((backend (eql :mezzano)) &rest args &key &allow-other-keys)
+  (apply #'make-instance 'window :fifo (mezzano.supervisor:make-fifo 50) args))
 
 (defmethod fb-int:list-displays-backend ((backend (eql :mezzano)))
   ;; TODO: implement list-displays-backend
   )
 
-(defclass window (fb:window)
-  ((buffer :initarg :buffer :initform NIL :reader fb:buffer :accessor buffer)))
+(defclass window (fb:window mezzano.gui.compositor:window)
+  ((buffer :initarg :buffer :initform NIL :reader fb:buffer :accessor buffer)
+   (frame :initform NIL :accessor frame)))
+
+(defmethod initialize-instance :after ((window window) &key title)
+  (setf (frame window) (make-instance 'mezzano.gui.widgets:frame
+                                      :framebuffer (mezzano.gui.compositor:window-buffer window)
+                                      :title (or title (fb-int:default-title))
+                                      :close-button-p T
+                                      :resizablep (fb:resizable-p window)
+                                      :damage-function (mezzano.gui.widgets:default-damage-function window)
+                                      :set-cursor-function (mezzano.gui.widgets:default-cursor-function window))))
+
+(defun update-buffer (window w h)
+  (let ((new-framebuffer (mezzano.gui:make-surface w h)))
+    (mezzano.gui.widgets:resize-frame (frame window) new-framebuffer)
+    (mezzano.gui.compositor:resize-window (window window) new-framebuffer))
+  (fb-int:resize-buffer w h (buffer window) (fb:width window) (fb:height window))
+  (setf (car (fb-int:size window)) w)
+  (setf (cdr (fb-int:size window)) h))
 
 (defmethod fb:valid-p ((window window))
   ;; TODO: implement valid-p
@@ -122,8 +139,24 @@
   )
 
 (defmethod fb:swap-buffers ((window window) &key (x 0) (y 0) (w (fb:width window)) (h (fb:height window)) sync)
-  ;; TODO: implement swap-buffers
-  )
+  ;; We have to re-encode to copy into the framebuffer. Very sad.
+  (loop with row-gap = (- (fb:width window) w)
+        with src = (fb:buffer window)
+        with dst = (mezzano.gui.compositor:window-buffer window)
+        with si = 0
+        with di = 0
+        for yi of-type (unsigned-byte 16) from y below (+ y h)
+        do (loop for xi of-type (unsigned-byte 16) from x below (+ x w)
+                 for px = (aref src (+ si 0))
+                 do (setf (ldb (byte 8  8) px) (aref src (+ si 1)))
+                    (setf (ldb (byte 8 16) px) (aref src (+ si 2)))
+                    (setf (ldb (byte 8 24) px) (aref src (+ si 3)))
+                    (setf (aref dst di) px)
+                    (incf si 4)
+                    (incf di 1))
+           (incf si (* 4 row-gap))
+           (incf di row-gap))
+  (mezzano.gui.compositor:damage-window window x y w h))
 
 (defmethod fb:process-events ((window window) &key timeout)
   ;; TODO: implement process-events
@@ -157,3 +190,46 @@
 (defmethod fb:video-mode ((display display))
   ;; TODO: implement video-mode
   )
+
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:window-activation-event))
+  (fb:window-focused window (mezzano.gui.compositor:state event)))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:mouse-event))
+  (handler-case (mezzano.gui.widgets:frame-mouse-event (frame window) event)
+    (mezzano.gui.widgets:close-button-clicked ()
+      (fb:window-closed window))))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:window-close-event))
+  (fb:window-closed window))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:quit-event))
+  (fb:window-closed window))
+
+(defun adjust-size ((window window) size)
+  (flet ((frob (x min max)
+           (if max
+               (max min (min x max))
+               (max min x))))
+    (setf (car size) (frob (car size) (car (fb:minimum-size window)) (car (fb:maximum-size window))))
+    (setf (cdr size) (frob (cdr size) (cdr (fb:minimum-size window)) (cdr (fb:maximum-size window))))
+    size))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:resize-request-event))
+  (let ((new-size (adjust-size window (cons (mezzano.gui.compositor:width event)
+                                            (mezzano.gui.compositor:height event)))))
+    (unless (equal new-size (fb:size window))
+      (update-buffer window (car new-size) (cdr new-size))
+      (fb:window-resized window (car new-size) (cdr new-size))
+      (fb:window-refreshed window))))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:resize-event))
+  (fb:window-refreshed window))
+
+(defmethod process-event ((window window) (event mezzano.gui.compositor:key-event))
+  (fb:key-changed window
+                  (mezzano.gui.compositor:key-key event)
+                  (if (mezzano.gui.compositor:key-releasep event)
+                      :release :press)
+                  NIL
+                  NIL))
